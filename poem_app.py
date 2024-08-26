07dc98cd2e3619a5001cdce75cacbad24f917901
Ward Greunke <wgreunke@gmail.com>
2024-08-26 14:32:00 -0700

    Create poem_app.py

---
+++ poem_app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+#This is the main streamlit app.  It takes in a set of poems and images and then shows them on the web.
+#A button allows you to advance to the next poem.  #Goal is to get two pages to see how navigation looks.
+
+#This is the core data that will be used for example.
+keywords=["knife","Apple","Table"]
+
+
+
+st.title("Let's Learn German!")
+
+
+
+
+st.write("What are the advantages of this method?")
