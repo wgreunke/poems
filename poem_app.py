db96b947814135fec28f37e0df51ea1a0c7e3a07
Ward Greunke <wgreunke@gmail.com>
2024-08-26 16:14:23 -0700

    Update poem_app.py

--- poem_app.py
+++ poem_app.py
@@ -8,7 +8,9 @@ keywords=["knife","Apple","Table"]
 #Target is the language that you want to learn.
 #Base is your current language.
 
-page_num=0
+if 'page_num' not in st.session_state:
+    st.session_state['page_num'] = 0
+
 target_lines=[["This is first line of target",
              "This is second line of target",
              "This is third line of target",
@@ -21,14 +23,14 @@ base_lines=["Base poem translation",
 
 
 st.title("Let's Learn German!")
-st.markdown(f"{target_lines[page_num][0]}<br>:gray[{base_lines[0]}]<br>{target_lines[page_num][1]}<br>:gray[{base_lines[1]}]<br>{target_lines[page_num][2]}<br>:gray[{base_lines[2]}]<br>{target_lines[page_num][3]}<br>:gray[{base_lines[3]}]",unsafe_allow_html=True )
+st.markdown(f"{target_lines[st.session_state['page_num']][0]}<br>:gray[{base_lines[0]}]<br>{target_lines[page_num][1]}<br>:gray[{base_lines[1]}]<br>{target_lines[page_num][2]}<br>:gray[{base_lines[2]}]<br>{target_lines[page_num][3]}<br>:gray[{base_lines[3]}]",unsafe_allow_html=True )
 
 #st.markdown(f"{target_lines[1][0]}<br>:gray[{base_lines[0]}]<br>{target_lines[1][1]}<br>:gray[{base_lines[1]}]<br>{target_lines[1][2]}<br>:gray[{base_lines[2]}]<br>{target_lines[1][3]}<br>:gray[{base_lines[3]}]",unsafe_allow_html=True )
 
 if st.button("Previous Page"):
-  page_num=page_num-1
+  st.session_date['page_num']=st.session_state['page_num']-1
 if st.button("Next Page"):
-  page_num=page_num+1
+  st.session_date['page_num']=st.session_state['page_num']+1
 
 
 st.write(page_num)
