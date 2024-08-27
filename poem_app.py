a0c13a148b953e60cb1edf3fce9d8b7e5ff047a9
Ward Greunke <wgreunke@gmail.com>
2024-08-26 17:50:27 -0700

    adding nested list to baselines

--- poem_app.py
+++ poem_app.py
@@ -12,10 +12,11 @@ target_lines=[["This is first line of target",
              "This is third line of target",
              "This is fourth line of target",],
               ["Second poem","sp1","sp2","sp3"]]
-base_lines=["Base poem translation",            
+base_lines=[["Base poem translation",            
            "Second line translate",
            "Third line",
-           "Fourth line"]
+           "Fourth line"],
+            ["a","B","C","D"]]
 st.title("Let's Learn German!")
 
 #Give the session state an initial value:
@@ -27,7 +28,7 @@ if 'count_value' not in st.session_state:
 #st.write(st.session_state['page_num'])
 st.write("count_value")
 st.write(st.session_state.count_value)
-st.markdown(f"{target_lines[st.session_state.count_value][0]}<br>:gray[{base_lines[0]}]<br>{target_lines[st.session_state.count_value][1]}<br>:gray[{base_lines[1]}]<br>{target_lines[st.session_state.count_value][2]}<br>:gray[{base_lines[2]}]<br>{target_lines[st.session_state.count_value][3]}<br>:gray[{base_lines[3]}]",unsafe_allow_html=True )
+st.markdown(f"{target_lines[st.session_state.count_value][0]}<br>:gray[{base_lines[st.session_state.count_value][0]}]<br>{target_lines[st.session_state.count_value][1]}<br>:gray[{base_lines[st.session_state.count_value][1]}]<br>{target_lines[st.session_state.count_value][2]}<br>:gray[{base_lines[st.session_state.count_value][2]}]<br>{target_lines[st.session_state.count_value][3]}<br>:gray[{base_lines[st.session_state.count_value][3]}]",unsafe_allow_html=True )
 
 #st.markdown(f"{target_lines[1][0]}<br>:gray[{base_lines[0]}]<br>{target_lines[1][1]}<br>:gray[{base_lines[1]}]<br>{target_lines[1][2]}<br>:gray[{base_lines[2]}]<br>{target_lines[1][3]}<br>:gray[{base_lines[3]}]",unsafe_allow_html=True )
 
