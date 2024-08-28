c94e5f36e37e2a02706f23ddf62d709ed9a3e03c
Ward Greunke <wgreunke@gmail.com>
2024-08-28 11:20:21 -0700

    Adding df into formated lines

--- poem_app.py
+++ poem_app.py
@@ -38,8 +38,11 @@ st.write(lines_df.at[0,"t_1"])
 light_text = "This is the text variable"
 bold_text="This is the bold text"
 
-st.write(f"""<span style='color: black; font-weight: bold;'>{light_text}</span><BR>
-<span style='color: lightgray; font-weight: bold;'>{bold_text}</span>
+st.write(f"""
+<span style='color: black; font-weight: bold;'>{lines_df.at[0,'t_1']}</span><BR>
+<span style='color: lightgray;'>{lines_df.at[0,'b_1']}</span>
+
+
 """,unsafe_allow_html=True)
 
          
