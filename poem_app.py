e5478da563265ac5ad2e9393b232edc440c9530d
Ward Greunke <wgreunke@gmail.com>
2024-08-28 12:09:28 -0700

    Starting the cleanup after moving to df.

--- poem_app.py
+++ poem_app.py
@@ -6,24 +6,14 @@ import pandas as pd
 #A button allows you to advance to the next poem.  #Goal is to get two pages to see how navigation looks.
 
 #This is the core data that will be used for example.
-keywords=["knife","Apple","Table"]
 #Target is the language that you want to learn.
 #Base is your current language.
-target_lines=[["This is first line of target",
-             "This is second line of target",
-             "This is third line of target",
-             "This is fourth line of target",],
-              ["Second poem","sp1","sp2","sp3"]]
-base_lines=[["Base poem translation",            
-           "Second line translate",
-           "Third line",
-           "Fourth line"],
-            ["a","B","C","D"]]
 
 #Create a dataframe using the lines.
 column_names=['key_words','t_lang','b_lang','t_1','t_2','t_3','t_4','b_1','b_2','b_3','b_4']
 
 #Create dummy data for the df.
+#This will be replaced by a sql call that converst the output to a df.
 row_3=[
   "Put keywords here",
   "German",
@@ -63,16 +53,17 @@ row_2=[
 "The bread is delicious, a treat,",
 "The dog is loyal and noisy."]
 
-
+#Combine the lines into a dataframe
 lines_df=pd.DataFrame([row_1,row_2],columns=column_names)
 st.write(lines_df)
-#st.write(lines_df.at[0,"t_1"])
 
-light_text = "This is the text variable"
-bold_text="This is the bold text"
-
+if 'count_value' not in st.session_state:
+    st.session_state.count_value = 0
 lines_row_num=1
 
+st.title("Let's Learn German!")
+
+#This is the main text output
 st.write(f"""
 <span style='color: black; font-weight: bold;'>{lines_df.at[lines_row_num,'t_1']}</span><BR>
 <span style='color: lightgray;'>{lines_df.at[lines_row_num,'b_1']}</span><BR>
@@ -82,27 +73,11 @@ st.write(f"""
 <span style='color: lightgray;'>{lines_df.at[lines_row_num,'b_3']}</span><BR>
 <span style='color: black; font-weight: bold;'>{lines_df.at[lines_row_num,'t_4']}</span><BR>
 <span style='color: lightgray;'>{lines_df.at[lines_row_num,'b_4']}</span><BR>
-
-
 """,unsafe_allow_html=True)
 
-         
-st.markdown(f"<span style='font-weight: bold; color: #B0B0B0;'>{light_text}</span>")
-st.markdown(f"<span style='color: #B0B0B0;'>{light_text}</span>")
-st.markdown(f"<span style='font-weight: bold; color: #B000000;'>{bold_text}</span>")
-
-
-#["Second poem","sp1","sp2","sp3"]]
-
-
-
-
-st.title("Let's Learn German!")
-
+        
 #Give the session state an initial value:
 
-if 'count_value' not in st.session_state:
-    st.session_state.count_value = 0
 
 
 #st.write(st.session_state['page_num'])
