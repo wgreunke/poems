abfe34fc5fbaaae343df2a9f01449dcaa0af7329
Ward Greunke <wgreunke@gmail.com>
2024-08-28 11:04:39 -0700

    Update poem_app.py

--- poem_app.py
+++ poem_app.py
@@ -37,6 +37,7 @@ st.write(lines_df.at[0,"t_1"])
 
 light_text = "This is the text variable"
 bold_text="This is the bold text"
+st.write(f"<span style='color: lightgray; font-weight: bold;'>{light_text}</span>", unsafe_allow_html=True)
 st.markdown(f"<span style='font-weight: bold; color: #B0B0B0;'>{light_text}</span>")
 st.markdown(f"<span style='color: #B0B0B0;'>{light_text}</span>")
 st.markdown(f"<span style='font-weight: bold; color: #B000000;'>{bold_text}</span>")
